17f3ffa5fd24de5254889d790df08a791d31d0a8
Piyapat Saranrittichai <p.saranrittichai@gmail.com>
2014-01-19 22:12:46 +0000

    db2

--- web.js
+++ web.js
@@ -14,8 +14,8 @@ app.get('/', function(req, res){
 app.get('/checkdb', function(req, res){
 	//connect to database
 	var connection_url = process.env.DATABASE_URL;
-	//var client = new pg.Client(connection_url);
-	var client = new pg.Client({host: 'localhost', database: 'MY_PG_DB'});
+	var client = new pg.Client({host: 'ec2-54-197-250-40.compute-1.amazonaws.com', port: '5432', user: 'yccajvsttfpvlr', password: 'mL2UfYiDoovxj-_2wkcMzxd5m3', database: 'd3guq0f238spmu'});
+	//var client = new pg.Client({host: 'localhost', database: 'MY_PG_DB'});
 	
 	client.connect();
 
