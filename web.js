8ccd5334efeb786280fa78c889fc0abe5ccddc87
Piyapat Saranrittichai <p.saranrittichai@gmail.com>
2014-01-19 21:10:31 +0000

    db

--- web.js
+++ web.js
@@ -3,13 +3,23 @@
 var express = require('express');
 var pg = require('pg');
 var app = express();
-
 var port = process.env.PORT || 5000;
+
+//connect to database
+var connection_url = process.env.DATABASE_URL;
+var client = new pg.Client(connection_url);
+client.connect();
 
 //Send text directly
 app.get('/', function(req, res){
-		//res.send('PostgreSQL-Test!');
-		res.send('My data : ' + process.env.DATABASE_URL);
+		res.send('PostgreSQL-Test!');
+	}
+);
+
+app.get('/checkdb', function(req, res){
+	var  query = client.query("SELECT * FROM users', [req.params.client_id]);
+   	query.on('row', function(row) {
+   		res.send(row);
 	}
 );
 
@@ -17,17 +27,3 @@ app.listen(port, function() {
  		console.log(" Listening on " + port);
  	}
 );
-
-function get_data(){
-	ret = '';
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-  		var query = client.query('SELECT * FROM users');
-  		query.on('row', function(row) {
-    		ret = JSON.stringify(row);
-    		console.log(JSON.stringify(row));
-  		});
-	});
-	
-	return ret;
-}
-
