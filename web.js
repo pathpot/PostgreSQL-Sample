127223500d19796c2dda7efc9116476979fc8180
Piyapat Saranrittichai <p.saranrittichai@gmail.com>
2014-01-19 21:12:35 +0000

    db2

--- web.js
+++ web.js
@@ -5,11 +5,6 @@ var pg = require('pg');
 var app = express();
 var port = process.env.PORT || 5000;
 
-//connect to database
-var connection_url = process.env.DATABASE_URL;
-var client = new pg.Client(connection_url);
-client.connect();
-
 //Send text directly
 app.get('/', function(req, res){
 		res.send('PostgreSQL-Test!');
@@ -17,6 +12,12 @@ app.get('/', function(req, res){
 );
 
 app.get('/checkdb', function(req, res){
+	//connect to database
+	var connection_url = process.env.DATABASE_URL;
+	var client = new pg.Client(connection_url);
+	client.connect();
+
+	% query
 	var  query = client.query("SELECT * FROM users', [req.params.client_id]);
    	query.on('row', function(row) {
    		res.send(row);
